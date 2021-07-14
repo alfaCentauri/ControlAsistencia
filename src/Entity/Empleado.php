0788e874a3835f44fb17dad5fb758b3e56c5e7ca
Ricardo Presilla <ricardopresilla@gmail.com>
2021-07-14 15:17:28 -0400

    Create crud for Empleado.

---
+++ src/Entity/Empleado.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmpleadoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmpleadoRepository::class)
+ */
+class Empleado
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cedula;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $apellido;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCedula(): ?int
+    {
+        return $this->cedula;
+    }
+
+    public function setCedula(int $cedula): self
+    {
+        $this->cedula = $cedula;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+}
