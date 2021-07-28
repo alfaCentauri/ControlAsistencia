d338f7ceb3fa2f92cbf4832f42168177bf8dc16d
Ricardo Presilla <ricardopresilla@gmail.com>
2021-07-27 22:44:47 -0400

    Update controller.

--- src/Controller/AsistenciaController.php
+++ src/Controller/AsistenciaController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Asistencia;
 use App\Form\AsistenciaType;
 use App\Repository\AsistenciaRepository;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
